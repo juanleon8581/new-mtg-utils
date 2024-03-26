cc4f53df3a658b4ce02ea3a73cc9c3f2a05b720f
Ardat <juanleon8581@gmail.com>
2024-03-26 12:47:53 -0500

    Update App Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./generic/ErrorPage/ErrorPage";
 import { routerStrings } from "./generic/globalStrings";
 import { Root } from "./Root";
-import { WelcomePage } from "./presentation/pages";
+import { TimerPage, WelcomePage } from "./presentation/pages";
 
 const routes = createBrowserRouter([
   {
@@ -14,6 +14,10 @@ const routes = createBrowserRouter([
       {
         path: `${routerStrings.root}`,
         element: <WelcomePage />,
+      },
+      {
+        path: `${routerStrings.timer}`,
+        element: <TimerPage />,
       },
     ],
   },
