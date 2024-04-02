ca801a36f3bf289e734d6275c3755fa4bd9d288e
Ardat <juanleon8581@gmail.com>
2024-04-02 03:29:19 -0500

    Add BMAC Component for support Project

---
+++ src/generic/BuyMeACoffe/BuyMeACoffe.tsx
@@ -0,0 +1,13 @@
+import "./BuyMeACoffe.css";
+
+export const BuyMeACoffe = () => {
+  return (
+    <a
+      className="buyButton"
+      target="_blank"
+      href="https://www.buymeacoffee.com/juanleon"
+    >
+      <span className="coffeeButtonText">☕ Buy me a coffee</span>
+    </a>
+  );
+};
